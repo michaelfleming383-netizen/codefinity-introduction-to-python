dfda201c81bb46cf50fa236b373cf6786cc966c5
michaelfleming383-netizen <>
2026-03-03 19:16:58 +0000

    Challenge: Product Revenues Capstone

--- functions/challenge_product_revenues_capstone/main.py
+++ functions/challenge_product_revenues_capstone/main.py
@@ -9,16 +9,16 @@ def calculate_revenue(prices, quantities_sold):
         new_price = prices[item] * quantities_sold[item]
         revenues.append(new_price)
     return revenues
-    #print("Step One Complete")
+# This function should multiply each price by its corresponding quantity sold, 
+# store the results in a list, and return this list of revenues.
     
 revenues = calculate_revenue(prices, quantities_sold)
-revenue_per_product = sorted(zip(products, revenues))
+revenue_per_product = sorted(zip(products, revenues)) #pairs each product with associated revenue and sorts alphabetically
 
 def formatted_output(revenue_per_product):
     for prod, rev in revenue_per_product:
         print(f"{prod} has total revenue of ${rev}")
 formatted_output(revenue_per_product)
-
-    
-# Example of expected output line (do not remove):
-# print(f"{revenue[0]} has total revenue of ${revenue[1]}")
+# This function should take a list of `(product_name, revenue)` tuples, 
+# sort them alphabetically by product name,
+# and print each in the specified format.
